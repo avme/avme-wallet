523d5f41b6028855ffba514f3c9b68d4d44ef78a
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2021-01-02 04:23:00 -0300

    Update secp256k1 download link

--- cmake/ProjectSecp256k1.cmake
+++ cmake/ProjectSecp256k1.cmake
@@ -14,8 +14,8 @@ ExternalProject_Add(
     PREFIX "${prefix}"
     DOWNLOAD_NAME secp256k1-ac8ccf29.tar.gz
     DOWNLOAD_NO_PROGRESS 1
-    URL https://github.com/itamarcps/secp256k1/archive/db1679fa15eaea1419f9229615c2838605857e29.tar.gz
-    URL_HASH SHA256=9d0a2b1fa56c924af0f962514ee69683f3666f262e274bba17056bec6e50c117
+    URL https://github.com/itamarcps/secp256k1/archive/0ac22b3fb07373d98b1873424c3438f0cbba6868.tar.gz
+    URL_HASH SHA256=d11e68c6ef733d970214e18bfe58d7ec9e8aa83136fda54fe582531067e0d169
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${CMAKE_CURRENT_LIST_DIR}/secp256k1/CMakeLists.txt <SOURCE_DIR>
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
