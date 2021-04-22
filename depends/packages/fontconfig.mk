4588b95b9c37137d5d9a33bf5d8399b89eea1e2d
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2021-04-21 23:37:38 -0300

    Depends: update Qt to 5.15.2, use only static libs

--- depends/packages/fontconfig.mk
+++ depends/packages/fontconfig.mk
@@ -7,7 +7,7 @@ $(package)_dependencies=freetype expat
 $(package)_patches=remove_char_width_usage.patch gperf_header_regen.patch
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-docs --disable-static --disable-libxml2 --disable-iconv
+  $(package)_config_opts=--disable-docs --enable-static --disable-libxml2 --disable-iconv
   $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
 endef
 
