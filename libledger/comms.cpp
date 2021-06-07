60131f6b7a3e9d31f664e1a96fc987ec10d0ddc5
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2021-06-07 11:25:56 -0300

    LEDGER: Loopify device check.

--- libledger/comms.cpp
+++ libledger/comms.cpp
@@ -21,12 +21,13 @@ namespace ledger {
     struct hid_device_info *devs;
     devs = hid_enumerate(0x0, 0x0);
     while (devs) {
-      if (devs->vendor_id == ledger_devices[0].vendor_id ||
-          devs->vendor_id == ledger_devices[1].vendor_id ) {
-        this->ledgerVID = devs->vendor_id;
-        hid_free_enumeration(devs);
-        hid_exit();
-        return true;
+      for (auto ledger_device : ledger_devices) {
+	    if (devs->vendor_id == ledger_device.vendor_id) {
+          this->ledgerVID = devs->vendor_id;
+          hid_free_enumeration(devs);
+          hid_exit();
+          return true;
+	    }
       }
       devs = devs->next;
     }
@@ -42,12 +43,13 @@ namespace ledger {
     struct hid_device_info *devs;
     devs = hid_enumerate(this->ledgerVID, 0x0);
     while (devs) {
-      if (devs->product_id == ledger_devices[0].product_id ||
-          devs->product_id == ledger_devices[1].product_id ) {
-        this->ledgerPID = devs->product_id;
-        hid_free_enumeration(devs);
-        hid_exit();
-        return true;
+      for (auto ledger_device : ledger_devices) {
+		if (devs->product_id == ledger_device.product_id) {
+		  this->ledgerPID = devs->product_id;
+		  hid_free_enumeration(devs);
+		  hid_exit();
+		  return true;
+		}
       }
       devs = devs->next;
     }
