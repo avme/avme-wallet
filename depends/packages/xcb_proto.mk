b8fb20bc1e54bac2e4cad6f15536858ad65544f3
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2021-06-30 21:24:49 -0300

    Add patch for python 3.9 on xcb_proto

--- depends/packages/xcb_proto.mk
+++ depends/packages/xcb_proto.mk
@@ -3,6 +3,11 @@ $(package)_version=1.14
 $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=xcb-proto-$($(package)_version).tar.xz
 $(package)_sha256_hash=186a3ceb26f9b4a015f5a44dcc814c93033a5fc39684f36f1ecc79834416a605
+$(package)_patches=xcbproto_python.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 -i $($(package)_patch_dir)/xcbproto_python.patch && \
+endef
 
 define $(package)_config_cmds
   $($(package)_autoconf)
