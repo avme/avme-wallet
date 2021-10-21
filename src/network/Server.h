b872d2edeb1491943373d63ac9bf19a9efbbe17d
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2021-10-21 11:02:31 -0300

    LIB: rework some comments on Server.h

--- src/network/Server.h
+++ src/network/Server.h
@@ -30,8 +30,6 @@ using tcp = boost::asio::ip::tcp;
 
 // Websocket async server, adapted from Boost docs:
 // https://www.boost.org/doc/libs/1_76_0/libs/beast/example/websocket/server/async/websocket_server_async.cpp
-// TODO: rework comments
-
 // Class "session" needs to be in it's own namespace in order to do forward declaration.
 
 // Handles all received WebSocket messages.
@@ -41,34 +39,47 @@ class session : public std::enable_shared_from_this<session> {
   beast::flat_buffer answerBuffer_;
   websocket::stream<beast::tcp_stream> ws_;
   // Pointer to list of sessions.
-  // Session needs access to it for inserting itself in the list
+  // Session needs access to it so it can insert itself in the list.
   std::unordered_set<std::shared_ptr<session>> *sessions_;
   std::mutex m_lock;  // Lock for thread safety.
 
   public:
+    // Take ownership of the socket.
+    explicit session(
+      tcp::socket&& socket,
+      std::unordered_set<std::shared_ptr<session>> *sessions,
+      QmlSystem *sys
+    ) : ws_(std::move(socket)), sessions_(sessions), sys_(sys) {}
+
+    /**
+     * Get on the correct executor.
+     * We need to execute within a strand to perform async operations
+     * in the I/O objects in this session.
+     * Although not strictly necessary for single-threaded contexts,
+     * this is written to be thread-safe by default.
+     */
     websocket::stream<beast::tcp_stream>::executor_type get_executor() { return ws_.get_executor(); };
 
-    // Take ownership of the socket.
-    explicit session(tcp::socket&& socket, std::unordered_set<std::shared_ptr<session>> *sessions, QmlSystem *sys) : ws_(std::move(socket)), sessions_(sessions), sys_(sys) {}
-    // Get on the correct executor.
-    // We need to be executing within a strand to perform async operations
-    // on the I/O objects in this session. Although not strictly necessary
-    // for single-threaded contexts, this example code is written to be
-    // thread-safe by default.
-    // Close the socket
+    // Close the socket.
     void close();
     void on_closed(beast::error_code ec);
-    void run();
+
     // Start the asynchronous operation.
+    void run();
     void on_run();
+
     // Read a message.
     void on_accept(beast::error_code ec);
+
     // Read a message into the buffer.
     void do_read();
+
     // Pass the message to the proper parser and listen for more.
     void on_read(beast::error_code ec, std::size_t bytes_transferred);
+
     // Send a message
     void do_write(std::string response);
+
     // Catch errors over writing a message.
     void on_write(beast::error_code ec, std::size_t bytes_transferred);
 };
@@ -76,24 +87,25 @@ class session : public std::enable_shared_from_this<session> {
 class Server {
   // Accepts incoming connections and launches the sessions.
   class listener : public std::enable_shared_from_this<listener> {
-    // Pointer to QmlSystem
-    QmlSystem* sys_;
+    QmlSystem* sys_;  // Pointer to QmlSystem
     net::io_context& ioc_;
     tcp::acceptor acceptor_;
-    // Pointer to the list of sessions
-    // Listener needs to hold that to pass to the session it creates
+    // Pointer to the list of sessions.
+    // Listener needs to hold that to pass to the session it creates.
     std::unordered_set<std::shared_ptr<session>> *sessions_;
-    // Pointer to the list of listeners
+    // Pointer to the list of listeners.
     // Listener needs access to it in order to insert itself.
     std::unordered_set<std::shared_ptr<listener>> *listeners_;
-    // Lock for thread safety.
-    std::mutex m_lock;
+    std::mutex m_lock;  // Lock for thread safety.
 
     public:
-      auto get_executor() { return acceptor_.get_executor(); };
       // Constructor.
-      listener(net::io_context& ioc, tcp::endpoint endpoint, std::unordered_set<std::shared_ptr<session>> *sessions, std::unordered_set<std::shared_ptr<listener>> *listeners, QmlSystem *sys)
-        : ioc_(ioc), acceptor_(ioc), sessions_(sessions), listeners_(listeners), sys_(sys) {
+      listener(
+        net::io_context& ioc, tcp::endpoint endpoint,
+        std::unordered_set<std::shared_ptr<session>> *sessions,
+        std::unordered_set<std::shared_ptr<listener>> *listeners,
+        QmlSystem *sys
+      ) : ioc_(ioc), acceptor_(ioc), sessions_(sessions), listeners_(listeners), sys_(sys) {
         beast::error_code ec;
         acceptor_.open(endpoint.protocol(), ec);  // Open the acceptor
         if (ec) { fail(ec, "open"); return; }
@@ -104,8 +116,9 @@ class Server {
         acceptor_.listen(net::socket_base::max_listen_connections, ec); // Start listening
         if (ec) { fail(ec, "listen"); return; }
       }
+      auto get_executor() { return acceptor_.get_executor(); };
 
-      // Start/stop accepting incoming connections/ respectively.
+      // Start/stop accepting incoming connections.
       void run();
       void stop();
 
@@ -113,29 +126,30 @@ class Server {
       // Handle connection acception.
       void do_accept();
 
-      // Create and run the session, then accept another connection
+      // Create and run the session, then accept another connection.
       void on_accept(beast::error_code ec, tcp::socket socket);
   };
 
   private:
     QmlSystem* sys_;
     net::io_context ioc;
-    // In order to send a boost::asio::post message to the thread.
-    // We need a object of it.
-    // For that reason, I have created a pointer which will pass through objects until
-    // it is inserted by the session itself.
-    // TODO: PROTECT THE LIST WITH MUTEX'ES
+    /**
+     * We need a thread object in order to send a post message to it,
+     * so we use a shared pointer here, which will pass through objects
+     * until it is inserted by the session itself.
+     * We also need a list of listeners, in order to close them all
+     * after the sessions are properly closed.
+     */
+    // TODO: protect the lists with mutexes
     std::unordered_set<std::shared_ptr<session>> sessions_;
-    // We also need a list of the listeners, in order to close them after the sessions are closed...
     std::unordered_set<std::shared_ptr<listener>> listeners_;
-    // TODO: This might be not the correct way to do this.
-    // But it was the way that I found... :shrug:
 
+    // The server's host, port and number of threads, respectively.
     boost::asio::ip::address address = boost::asio::ip::make_address("127.0.0.1");
     unsigned short port = 4812;
     unsigned short threads = 8;
 
-    // Mutex to wait until server was succesfully stopped
+    // Mutex to wait until server was succesfully stopped.
     std::mutex running;
 
   public:
@@ -149,7 +163,7 @@ class Server {
     // Set a pointer to the QmlSystem object.
     void setQmlSystem(QmlSystem* sys);
 
-    // Set a port to the server
+    // Set a port to the server.
     void setPort(unsigned short desiredPort) { this->port = desiredPort; };
 
     // Report a failure.
